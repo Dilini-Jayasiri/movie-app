90c1fa0d7f3b17d7eebc0c476be1867b3afa639e
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2024-01-23 19:19:36 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './component/tool-bar/tool-bar.component';
+import { MoviesHomeComponent } from './component/movies-home/movies-home.component';
+import { AddMovieComponent } from './component/add-movie/add-movie.component';
+import { ButtonComponent } from './component/button/button.component';
+import { MovieTileComponent } from './component/movie-tile/movie-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    MoviesHomeComponent,
+    AddMovieComponent,
+    ButtonComponent,
+    MovieTileComponent
   ],
   imports: [
     BrowserModule,
